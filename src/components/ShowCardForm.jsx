0286e194d2bf2f0de259b67e64ae74e4de0f1313
mohit <mohitpardeshi729@gmail.com>
2023-04-15 14:54:33 +0530

    merged conflict caused error : fixed now

--- src/components/ShowCardForm.jsx
+++ src/components/ShowCardForm.jsx
@@ -41,7 +41,7 @@ const handleSubmit=async(event)=> {
     setLoading(true);
     try {
       const response = await fetch(
-        'https://fitnesswebapp-backend-production.up.railway.app/users/login',
+        'https://fitnesswebapp-backend-production.up.railway.app/login',
         {
           method: 'POST',
           headers: {
